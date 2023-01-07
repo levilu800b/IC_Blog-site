70b84ee3bdce90372a2c319cb83d69688e16647d
levilu800b <levilu800b@gmail.com>
2023-01-07 14:41:23 +0000

    image changes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,34 +1,33 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
 	const posts = [
-	  {
-	    id: 1,
-	    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-	    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
-	    img: "https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-	  },
-	  {
-	    id: 2,
-	    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-	    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
-	    img: "https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-	  },
-	  {
-	    id: 3,
-	    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-	    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
-	    img: "https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-	  },
-	  {
-	    id: 4,
-	    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-	    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
-	    img: "https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-	  },
-   ];
-
+		{
+			id: 1,
+			title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+			desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
+			img: 'https://images.unsplash.com/photo-1673010960635-d0d1ad81b90a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+		},
+		{
+			id: 2,
+			title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+			desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
+			img: 'https://images.unsplash.com/photo-1672858502748-fb7dc81ef830?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxODd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+		},
+		{
+			id: 3,
+			title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+			desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
+			img: 'https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+		},
+		{
+			id: 4,
+			title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+			desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
+			img: 'https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+		},
+	];
 
 	return (
 		<div className="home">
@@ -39,7 +38,7 @@ const Home = () => {
 							<img src={post.img} alt="" />
 						</div>
 						<div className="content">
-							<Link className='link' to={`/post/${post.id}`}>
+							<Link className="link" to={`/post/${post.id}`}>
 								<h1>{post.title}</h1>
 							</Link>
 							<p>{post.desc}</p>
@@ -50,6 +49,6 @@ const Home = () => {
 			</div>
 		</div>
 	);
-}
+};
 
-export default Home
+export default Home;
